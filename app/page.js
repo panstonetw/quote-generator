9e1cc0fca72a0263e63d70d83760c515b3fbfc17
john <john870626@gmail.com>
2026-03-25 23:27:24 +0800

    fix: page.js 使用 FormProvider 方便子組件使用 useForm 的狀態

--- app/page.js
+++ app/page.js
@@ -3,58 +3,23 @@
 import React, {useState} from 'react';
 import {
     Box,
-    Button,
     Container,
-    Flex,
-    Heading,
     HStack,
     VStack,
-    Text,
-    Input,
     SimpleGrid,
-    Icon,
-    Separator,
-    Alert,
-    Collapsible,
-    FileUpload,
-    InputGroup,
     Tabs,
-    Stack,
-    NativeSelect,
-    Link,
-    Textarea
 } from '@chakra-ui/react';
-import {
-    Building2,
-    ChevronDown,
-    ChevronUp,
-    Copy, DollarSign,
-    Eye,
-    FileText,
-    Mail,
-    MapPin,
-    PanelRightClose,
-    Phone,
-    Plus,
-    ScrollText,
-    Trash2,
-    User
-} from "lucide-react";
-import { LuGithub, LuShoppingCart, LuUpload } from "react-icons/lu";
-import { CustomDatePicker } from "@/components/form/CustomDatePicker";
-import { FloatingInput } from "@/components/form/FloatingInput";
-import { CustomNumberInput } from "@/components/form/CustomNumberInput";
-import { CustomTable } from "@/components/data/CustomTable";
-import { CustomCheckbox } from "@/components/form/CustomCheckbox";
-import { CustomTooltip } from "@/components/text/CustomTooltip";
 import QuotePageHeader from "@/components/quote/QuotePageHeader";
 import QuotePreviewPanel from "@/components/quote/QuotePreviewPanel";
 import QuoteBasicInfoSection from "@/components/quote/QuoteBasicInfoSection";
 import QuoteItemsSection from "@/components/quote/QuoteItemsSection";
 import QuoteExtraInfoSection from "@/components/quote/QuoteExtraInfoSection";
+import { FormProvider, useForm } from "react-hook-form";
 
 export default function QuoteGeneratorChakra() {
-    const [open, setOpen] = useState(false);
+    const [displayPreview, setDisplayPreview] = useState(false);
+    const methods = useForm();
+
     // 色彩配置
     const formBg = {base: 'gray.200'};
     const inputBg = {base: 'white'};
@@ -80,8 +45,8 @@ export default function QuoteGeneratorChakra() {
 
                 {/* Tab Switcher */}
                 <HStack mb={4} p={1} bg="white" border="1px" borderColor="gray.200" borderRadius="xl" shadow="sm"
-                        w="fit-content">
-                    <Tabs.Root defaultValue="quote">
+                        w="fit-content" style={{ width: '100%'}}>
+                    <Tabs.Root defaultValue="quote" style={{ width: '100%'}}>
                         <Tabs.List>
                             <Tabs.Trigger value="quote">
                                 報價單
@@ -91,25 +56,32 @@ export default function QuoteGeneratorChakra() {
                             </Tabs.Trigger>
                         </Tabs.List>
                         <Tabs.Content value="quote" p={2}>
-                            <SimpleGrid columns={{base: 1, lg: 2}} spacing={6} templateColumns={{lg: '1.2fr 1fr'}}
-                                        gap={6}>
-                                <VStack spacing={6} align="stretch" flex={1}>
-                                    <QuoteBasicInfoSection
-                                        open={open}
-                                        setOpen={setOpen}
-                                        formBg={formBg}
-                                        inputBg={inputBg}
-                                        borderColor={borderColor}
-                                    />
+                            <FormProvider {...methods}>
+                                <form>
+                                    <SimpleGrid columns={displayPreview ? 2 : 1} spacing={6} templateColumns={displayPreview && {lg: '1.2fr 1fr'}}
+                                                gap={6}>
+                                        <VStack spacing={6} align="stretch" flex={1}>
+                                            <QuoteBasicInfoSection
+                                                onHidePreview={() => setDisplayPreview(!displayPreview)}
+                                                formBg={formBg}
+                                                inputBg={inputBg}
+                                                borderColor={borderColor}
+                                            />
 
-                                    <QuoteItemsSection />
+                                            <QuoteItemsSection />
 
-                                    <QuoteExtraInfoSection inputBg={inputBg} />                                                             
-                                </VStack>
+                                            <QuoteExtraInfoSection inputBg={inputBg} />
+                                        </VStack>
 
-                                {/* 報價單預覽 */}
-                                <QuotePreviewPanel columns={columns} data={data} />
-                            </SimpleGrid>
+                                        {/* 報價單預覽 */}
+                                        { displayPreview && (
+                                            <VStack>
+                                                <QuotePreviewPanel columns={columns} data={data} />
+                                            </VStack>
+                                        ) }
+                                    </SimpleGrid>
+                                </form>
+                            </FormProvider>
                         </Tabs.Content>
                     </Tabs.Root>
                 </HStack>
